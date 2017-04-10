0d984dc47a0ca658463eb01049c8864989d38f65
gelo0 <gelo0@o2.pl>
2017-04-10 16:49:14 +0200

    Done 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+
+  def title
+    exceptions = ['and', 'in', 'of', 'the', 'a', 'an']
+    final = []
+    title_a =
+    @title.split(' ').each do |word|
+      if exceptions.include? word
+        final.push(word)
+      else
+        final.push(word.capitalize)
+      end
+    end
+    final[0] = final[0].capitalize
+    return final.join(' ')
+  end
 end
