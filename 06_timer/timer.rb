21949ac7b741934a14d605207f912dff08c99799
gelo0 <gelo0@o2.pl>
2017-04-11 17:26:37 +0200

    Done 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,31 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+  def time_string
+    time_a = [@seconds/3600, (@seconds%3600)/60, @seconds%60]
+    time_s = []
+    i = 0
+    3.times do
+      if time_a[i] < 10
+        time_s.push(padded(time_a[i]))
+        i = i + 1
+      else
+        time_s.push(padded(time_a[i]))
+        i = i + 1
+      end
+    end
+
+    return time_s.join(':')
+  end
+  def padded(number)
+    if number < 10
+      return "0" + number.to_s
+    else
+      return number.to_s
+    end
+  end
 end
